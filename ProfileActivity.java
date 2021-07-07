303b159eac8cd2f2252c36874fe4639ef7ee883c
HOGdR <69900906+HOGdR@users.noreply.github.com>
2021-07-07 10:20:04 +0200

    Add files via upload

---
+++ ProfileActivity.java
@@ -0,0 +1,98 @@
+package com.example.logindemo;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView profilePic;
+    private TextView profileName,profileAge,profileEmail;
+    private Button profileUpdate,changePassword;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseStorage firebaseStorage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        profilePic = findViewById(R.id.ivProfilePic);
+        profileName = findViewById(R.id.tvProfileName);
+        profileAge = findViewById(R.id.tvProfileAge);
+        profileEmail = findViewById(R.id.tvProfileEmail);
+        profileUpdate = findViewById(R.id.btnProfileUpdate);
+        changePassword = findViewById(R.id.btnChangePassword);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+
+        DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+
+        StorageReference storageReference = firebaseStorage.getReference();
+        storageReference.child((firebaseAuth.getUid())).child("Images/Profile Pic").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+
+
+
+            }
+        });
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                UserProfile userProfile = snapshot.getValue(UserProfile.class);
+                profileName.setText("Name: " + userProfile.userName);
+                profileAge.setText("Age: " + userProfile.userAge);
+                profileEmail.setText("Email : " + userProfile.userEmail);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this,error.getCode(),Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        profileUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this, UpdateProfile.class));
+
+            }
+        });
+
+        changePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProfileActivity.this,UpdatePassword.class));
+            }
+        });
+
+
+    }
+}
